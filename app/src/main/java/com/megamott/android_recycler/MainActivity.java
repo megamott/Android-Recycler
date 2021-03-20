df41ac187a4c10d45ff524b65d9de91e78ccb265
megamott <megamott27@gmail.com>
2021-03-20 09:30:31 +0300

    Moved recycler view to the fragment.

--- app/src/main/java/com/megamott/android_recycler/MainActivity.java
+++ app/src/main/java/com/megamott/android_recycler/MainActivity.java
@@ -1,39 +1,14 @@
 package com.megamott.android_recycler;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity implements NumberAdapter.ItemClickListener{
-
-    private RecyclerView numberSheet;
-    private NumberAdapter numberAdapter;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-    }
-
-    private void init(){
-        numberSheet = findViewById(R.id.number_sheet);
-        numberSheet.setLayoutManager(new GridLayoutManager(this, 3));
-        numberAdapter = new NumberAdapter(20, this);
-        numberAdapter.setClickListener(this::onItemClick);
-        numberSheet.setAdapter(numberAdapter);
-    }
-
-    public void onClickInsert(View view){
-        numberAdapter.insert();
-    }
-
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, String.valueOf(position + 1), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/megamott/android_recycler/NumbersListFragment.java
+++ app/src/main/java/com/megamott/android_recycler/NumbersListFragment.java
@@ -2,18 +2,48 @@ package com.megamott.android_recycler;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class NumbersListFragment extends Fragment {
+public class NumbersListFragment extends Fragment implements NumberAdapter.ItemClickListener{
+
+    private RecyclerView numberSheet;
+    private NumberAdapter numberAdapter;
+    private Button button;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numbers_list, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_numbers_list, container, false);
+
+        numberSheet = view.findViewById(R.id.number_sheet);
+        numberSheet.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        numberAdapter = new NumberAdapter(20, getActivity());
+        numberAdapter.setClickListener(this::onItemClick);
+        numberSheet.setAdapter(numberAdapter);
+
+        button = view.findViewById(R.id.insertion_button);
+        button.setOnClickListener(v -> numberAdapter.insert());
+        return view;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), String.valueOf(position + 1), Toast.LENGTH_SHORT).show();
     }
 }
